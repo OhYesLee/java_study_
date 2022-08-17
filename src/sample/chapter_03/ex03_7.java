ff8d6909386cb2337902ed8d9161f8bfd2d8a12b
134741 <103974953+134741@users.noreply.github.com>
2022-08-17 18:10:48 +0900

    test20220817_3

---
+++ src/sample/chapter_03/ex03_7.java
@@ -0,0 +1,9 @@
+package sample.chapter_03;
+
+public class ex03_7 {
+    public static void main(String[] args) {
+        System.out.println(5 / 2);
+        System.out.println(5 / (float) 2); // (float)5/2 의 결과도 동일한 값
+
+    }
+}
