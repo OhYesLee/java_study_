5e9ddde2fcf3d1f2d8c62a682fecbdfabe7ec285
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-09-16 09:37:38 +0900

    20220916

---
+++ src/sample/java_test/test17.java
@@ -0,0 +1,19 @@
+package sample.java_test;
+
+import java.util.Scanner;
+
+/*
+8393번
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+ */
+public class test17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = 0;
+        for (int i = 0; i <= n; i++) {
+            r += i;
+        }
+        System.out.println(r);
+    }
+}
