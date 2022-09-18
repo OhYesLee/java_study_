0412523414bfe643ada2028aa8ffe1e1806e04ab
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-09-18 23:28:18 +0900

    20220918

---
+++ src/sample/test/test23.java
@@ -0,0 +1,20 @@
+package sample.test;
+
+import java.util.Scanner;
+
+/*
+2438번
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ */
+public class test23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/sample/test/test24.java
@@ -0,0 +1,22 @@
+package sample.test;
+
+import java.util.Scanner;
+
+/*
+10871번
+정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+ */
+public class test24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int a[] = new int[N];
+        int X = sc.nextInt();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+            if (a[i] < X) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
