9f29dd867f247e3f9404a36a91253ac439449cdd
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-01 23:46:42 +0900

    20221001

---
+++ tem_test_1/src/sample/test_1/test05.java
@@ -0,0 +1,17 @@
+package sample.test_1;
+
+import java.util.Scanner;
+
+public class test05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
