a31c56e12189a9c29684c5d28c0375ef7ea658e0
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-03 19:37:56 +0900

    20221003

---
+++ study_3/src/sample/chapter_04/ex04_17.java
@@ -0,0 +1,11 @@
+package sample.chapter_04;
+
+public class ex04_17 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 3 == 0) // 3의 배수이며 continue로 반복문 블럭의 끝으로 이동
+                continue;
+            System.out.println(i);
+        }
+    }
+}
