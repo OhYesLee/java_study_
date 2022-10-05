1027f7c6afd6d4eca559f77fc0d20881772be493
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-05 13:40:44 +0900

    20221005change

---
+++ Study4/src/sample/chapter_02_01/ex02_3.java
@@ -0,0 +1,15 @@
+package sample.chapter_02_01;
+
+public class ex02_3 {
+    public static void main(String[] args) {
+        System.out.println(5 + 3);
+        System.out.println(5 - 3);
+        System.out.println(5 * 3);
+        System.out.println(5 / 3);
+
+
+        System.out.println(5 % 3);
+        System.out.println(5 * 3);
+    }
+}
+
---
+++ Study4/src/sample/chapter_03/ex03_2.java
@@ -0,0 +1,15 @@
+package sample.chapter_03;
+
+public class ex03_2 {
+    public static void main(String[] args) {
+        int i = 5, j = 0;
+        j = i++;
+        System.out.println("j=i++; 실행 후, i = " + i + ",j = " + j);
+        i = 5;
+        j = 0;
+
+        j = ++i;
+        System.out.println("j=++i 실행 후, i = " + i + ", j = " + j);
+
+    }
+}
