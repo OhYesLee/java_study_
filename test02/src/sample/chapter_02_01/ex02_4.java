3c1d346d67072429aa4abb40a3d8c06ff9fd7f9d
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-06 20:43:52 +0900

    20221006

---
+++ test02/src/sample/chapter_02_01/ex02_4.java
@@ -0,0 +1,12 @@
+package sample.chapter_02_01;
+
+public class ex02_4 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        x = 10;
+        System.out.println(x);
+
+    }
+}
