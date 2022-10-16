daf6dea48aa591c4af1b24701f4cc915ee93591f
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-16 17:50:44 +0900

    20221016

---
+++ test07/src/ch01/ex03_1.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class ex03_1 {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = y = 3;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
