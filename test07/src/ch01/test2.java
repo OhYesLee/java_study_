801c3f8315f9f25f50c454d9a185b3d6a5d64039
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-16 17:51:23 +0900

    20221016

---
+++ test07/src/ch01/test2.java
@@ -0,0 +1,24 @@
+package ch01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+10818번
+
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ */
+public class test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[arr.length - 1]);
+
+    }
+}
