5d6fec7d42fb963bead956b999fc3c949eee68ca
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-16 17:51:40 +0900

    20221016

---
+++ test07/src/ch01/test6.java
@@ -0,0 +1,39 @@
+package ch01;
+
+import java.util.Scanner;
+
+/*
+1546번
+세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다.
+이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+ */
+public class test6 {
+    public static void main(String[] args) {
+		/*
+		 	두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
+			수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sub[] = new int[N];
+        double M = 0;
+        for (int i = 0; i < sub.length; i++) {
+            sub[i] = sc.nextInt();
+        }
+        for (int i = 0; i < sub.length; i++) {
+            if (M < sub[i]) {
+                M = (double) sub[i];
+                i--;
+            }
+        }
+        double newSub[] = new double[N];
+        double sum = 0;
+        for (int i = 0; i < sub.length; i++) {
+            newSub[i] = (double) sub[i] / M * (double) 100;
+            sum = newSub[i] + sum;
+        }
+        System.out.println(sum / (double) N);
+    }
+}
---
+++ test07/src/ch01/test8.java
@@ -0,0 +1,50 @@
+package ch01;
+
+import java.util.Scanner;
+
+/*
+4344번
+
+대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+%.3f 소수점 3자리까지 출력
+ */
+public class test8 {
+    public static void main(String[] args) {
+		/*
+		 	첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+			둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고,
+
+			이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int numberPeople, sum, overPeople;
+        double r, per;
+
+        for (int i = 0; i < count; i++) {
+            numberPeople = sc.nextInt();
+            sum = 0;
+            overPeople = 0;
+            r = 0;
+
+            int score[] = new int[numberPeople];
+            for (int j = 0; j < score.length; j++) {
+                score[j] = sc.nextInt();
+                sum = sum + score[j];
+            }
+            for (int j = 0; j < score.length; j++) {
+                if (score[j] > (double) sum / numberPeople) {
+                    overPeople++;
+                }
+            }
+
+            r = (double) overPeople / numberPeople * 100;
+
+            per = Math.round(r * 1000) / 1000.0;
+
+            System.out.printf("%.3f", per);
+            System.out.println("%");
+        }
+    }
+}
