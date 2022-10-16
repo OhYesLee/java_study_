531107c6a2ec19a9f34364cc22868d3fd3aa34d6
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-16 17:52:15 +0900

    20221016

---
+++ test07/src/ch01_01/Solution.java
@@ -0,0 +1,4 @@
+package ch01_01;
+
+public class Solution {
+}
---
+++ test07/src/ch01_01/test4.java
@@ -0,0 +1,34 @@
+package ch01_01;
+
+import java.util.Scanner;
+
+/*
+2577번
+세 개의 자연수 A, B, C가 주어질 때 A×B×C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+예를 들어 A = 150, B = 266, C = 427 이라면
+A × B × C = 150 × 266 × 427 = 17037300 이 되고,
+계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+ */
+public class test4 {
+    public static void main(String[] args) {
+        		/*
+		 	세 개의 자연수 A, B, C가 주어질 때 A×B×C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+			예를 들어 A = 150, B = 266, C = 427 이라면
+			A × B × C = 150 × 266 × 427 = 17037300 이 되고,
+			계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int r = n1 * n2 * n3;
+        int arr[] = new int[10];
+        while (r != 0) {
+            arr[r % 10]++;
+            r /= 10;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
