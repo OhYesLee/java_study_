1f8aa697f4ff7d6f5210d41dcd217922cf329c60
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-16 17:50:23 +0900

    20221016

---
+++ test07/src/ch01/Ex2_3.java
@@ -0,0 +1,10 @@
+package ch01;
+
+class Ex2_3 {
+	public static void main(String args[]) {
+		System.out.println(5+3);     // ȭ�鿡 5+3�� ����� 8�� ��µȴ�.
+		System.out.println(5-3);     // ȭ�鿡 5-3�� ����� 2�� ��µȴ�.
+		System.out.println(5*3);     // ȭ�鿡 5*3�� ����� 15�� ��µȴ�.
+		System.out.println(5/3);     // ȭ�鿡 5/3�� ����� 1�� ��µȴ�.
+	}
+}
